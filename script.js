1695c9d0e84b84631c3e926422582f8ee6e407ac
felicio <felicio@mobileit.co.jp>
2024-01-13 15:46:04 +0900

    edit message

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you there!";
+  question.innerHTML = "Yay, see you in Tokyo Dome!";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
     yesBtn.style.visibility='hidden';
